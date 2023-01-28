bde176bb83c86b4f0e02bfbac564eb59d914f133
abolfazlparadox <104286799+Abolfazlparadox@users.noreply.github.com>
2023-01-28 10:37:37 +0330

    idk

--- paradox.py
+++ paradox.py
@@ -1,2 +1,35 @@
+import sys
 def square(x):
     return x * x
+#decorators
+def a(f):
+    def w():
+        print("About to run the function...")
+        f()
+        print("Don with the function.")
+    return w
+@a
+def hello():
+    print("hello")
+
+people= [
+    {"name":"a", "house":"t"},
+    { "name":"f", "house":"r"},
+    { "name":"c", "house":"b"}
+]
+#def f(person):
+#    return person["name"]
+#people.sort(key=f)
+people.sort(key=lambda person: person["name"])
+print(people)
+
+x= int(input())
+y= int(input())
+
+try:
+   print(result =x/y)
+    
+except ZeroDivisionError:
+    print ("Error : Cannot divide by zero")
+    sys.exit(1)
+
--- paradox2.py
+++ paradox2.py
@@ -1,4 +1,22 @@
 from paradox import square
-
+class  point():
+    def __init__(self,input1,input2):
+        self.x = input1
+        self.y = input2
+class flight():
+    def __init__(self,capacity):
+        self.capacity = capacity
+        self.passengers=[]
+    def add_passengers(self,name):
+        if self.open():
+            return False
+        self.passengers.append(name)
+        return True
+    def open(self):
+        return self.capacity - len(self.passengers)
 for i in range(10):
     print(f"the square of {i} is {square(i)}")
+p=point(2,8)
+print(p.x)
+print(p.y)
+flight1=flight(4)
